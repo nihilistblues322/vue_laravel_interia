7247bebe6caab3f250ee4054ba8c0122d1f0a607
msc <miserylovescompany228@gmail.com>
2024-05-24 13:19:39 +0500

    layout

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header class="bg-slate-400 text-white">
+            <nav class="flex items-center justify-between p-4 mx-auto">
+                <div class="space-x-4">
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                </div>
+            </nav>
+        </header>
+        <main class="p-4">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup></script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,4 +2,7 @@
     <div>hello</div>
 </template>
 
-
+<script setup>
+import Layout from "./../Layouts/Layout.vue";
+defineOptions({ layout: Layout });
+</script>
