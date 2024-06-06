38bb9e11977921e599420feff719b4e06b743c9c
msc <miserylovescompany228@gmail.com>
2024-06-06 15:29:10 +0500

    progress indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,18 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .component('Link', Link)
-            .component('Head', Head)
+            .component("Link", Link)
+            .component("Head", Head)
             .mount(el);
+    },
+    progress: {
+        // The color of the progress bar...
+        color: "#29d",
+
+        // Whether to include the default NProgress styles...
+        includeCSS: true,
+
+        // Whether the NProgress spinner will be shown...
+        showSpinner: false,
     },
 });
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    sleep(0.3);
     return Inertia::render('Home');
 });
 
 Route::get('/about', function () {
+    sleep(0.3);
     return Inertia::render('About');
 });
